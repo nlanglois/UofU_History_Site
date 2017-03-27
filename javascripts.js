4f5fa326a4cda96208e91d8d8b7ee3a7a943fafc
Nikolai Langlois <nlangloi10@umphysicians.umn.edu>
2017-03-27 15:22:46 -0500

    Adding more content

---
+++ javascripts.js
@@ -0,0 +1,10 @@
+/**
+ * Created by nlangloi10 on 1/17/17.
+ */
+
+
+console.log("I logged something to the console!");
+
+console.groupCollapsed("Minion");
+console.log("Minion stuff");
+console.groupEnd();
